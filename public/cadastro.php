c8b53c26320d0a27b00f9ca5c0713cfd56e3e224
Isabella Calazans <57814393+isabellacalazans@users.noreply.github.com>
2023-05-05 11:08:36 -0300

    Initial commit

---
+++ public/cadastro.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TecSoft - Cadastro</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login_cad.css">
+</head>
+<body>
+    
+    <div class="">
+
+    </div>
+
+    <div class="container">
+        <div class="field">
+            <form action="">
+                <h1>Cadastro</h1> <br>
+                <label for="">Nome de usuário:</label> <br>
+                <input type="text" class="input" placeholder="Digite o nome de usuário..."> <br>
+                <label for="">Email:</label> <br>
+                <input type="text" class="input" placeholder="Digite seu email..."> <br>
+                <label for="">Senha</label> <br>
+                <input type="password" class="input" placeholder="Digite sua senha..."> <br> <br>
+                <input type="submit" value="Entrar"> <br> <br>
+                <a href="index.php">Entrar na sua conta</a>
+
+            </form>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TecSoft - Login</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login_cad.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="field">
+            <form action="">
+                <h1>Login</h1> <br>
+                <label for="">Email:</label> <br>
+                <input type="text" class="input" placeholder="Digite seu email..."> <br>
+                <label for="">Senha</label> <br>
+                <input type="password" class="input" placeholder="Digite sua senha..."> <br> <br>
+                <input type="submit" value="Entrar"> <br> <br>
+                <a href="cadastro.php">Primeiro acesso?</a>
+            </form>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
